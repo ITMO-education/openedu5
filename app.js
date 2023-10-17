e45a4300d308532a87ba60aa1e47563750efdc15
AlexSkilled <olegthander@yandex.ru>
2023-10-17 12:00:23 +0300

    app js

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 export function initApp(express, bodyParser, createReadStream, crypto, http) {
 	const app = express()
 
-	app.use(
-		bodyParser.urlencoded({
-			extended: true
-		}))
+	const CORS = {
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,OPTIONS,DELETE',
+		'Access-Control-Allow-Headers': 'Content-Type, Accept, Access-Control-Allow-Headers',
+		'Content-Type': 'text/plain; charset=utf-8'
+	};
+
+	const login = 'itmo370702';
+
+	app
+		.use(
+			bodyParser.urlencoded({extended: true})
+		)
+		.use(
+			bodyParser.json()
+		)
 		.use(function (req, res, next) {
-			res.setHeader('Access-Control-Allow-Origin', '*')
-			res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,OPTIONS,DELETE')
+			res.set(CORS)
 			next();
 		})
-		.get('/login/', (req, res) => res.send('itmo370702'))
+		.get('/login/', (req, res) => res.send(login))
+
 		.get('/code/', (req, res) => createReadStream(import.meta.url.substring(7)).pipe(res))
-		.get('/sha1/:input/', (req, res) => res.send(crypto.createHmac('sha1', "").update(req.params.input).digest('hex')))
+
+		.get('/sha1/:input/', (req, res) => {
+			res.send(
+				crypto.createHash('sha1', "").update(req.params.input).digest('hex')
+			)
+		})
+
 		.get('/req/', (req, res) => {
 			http.get(req.query.addr, (r) => {
 				r.on('data', (chunk) => {
@@ -24,18 +42,18 @@ export function initApp(express, bodyParser, createReadStream, crypto, http) {
 			})
 		})
 
-	.post('/req/', (req, res) => {
-		http.get(req.body.addr, (r) => {
-			r.on('data', (chunk) => {
-				res.write(chunk)
-			})
-			r.on('end', () => {
-				res.end()
+		.post('/req/', (req, res) => {
+			http.get(req.body.addr, (r) => {
+				r.on('data', (chunk) => {
+					res.write(chunk)
+				})
+				r.on('end', () => {
+					res.end()
+				})
 			})
 		})
-	})
 		.all('/*', (req, res) => {
-			res.end('itmo370702')
+			res.end(login)
 		})
 
 	return app
